85b7502ac42732125eb910e67d05e2d8882afce8
JoyMaz <mazumdarjoy941@gmail.com>
2026-03-31 10:38:17 +0530

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "resource_group_name" {
-  type = string
-}
-variable "location" {
-  type = string
-}
-variable "username" {
-  type = string
-}
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "username"            { type = string }
